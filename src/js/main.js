ec46f3bbf65fbc3adfc2d79a8a49a8a7640b6985
Nadia Ridko <nadia.ridko@gmail.com>
2018-08-08 21:52:39 +0200

    chage js

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,16 @@ const menuBth = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State of Menu
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBth.addEventListener('click', toggleMenu);
 
-function toggleMenu {
+function toggleMenu() {
   if(!showMenu) {
-    menuBtn.classList.add('close');
+    menuBth.classList.add('close');
     menu.classList.add('show');
     menuNav.classList.add('show');
     menuBranding.classList.add('show');
@@ -22,7 +22,7 @@ function toggleMenu {
     // Set Menu State
     showMenu = true;
   }else {
-    menuBtn.classList.remove('close');
+    menuBth.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
     menuBranding.classList.remove('show');
